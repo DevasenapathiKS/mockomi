36fcb2897cee37795fd9bc58188f29a5ec956341
Devasenapathi <65939943+Devasenapathi@users.noreply.github.com>
2026-01-15 01:21:34 +0530

    new build for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'https://mockomi.com',
+                target: 'https://server.mockomi.com',
                 changeOrigin: true,
                 secure: false,
             },
